71befe40ee74d369beacc7bf00388f633452c7db
nabin <lamanabin64@gmail.com>
2023-04-05 09:46:54 +0545

    changing div circular

---
+++ changing_div_circular.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+//hook
+const App = () => {
+    const [color, setColor] = useState('red')
+    const [height, setHeight] = useState(10)
+    const [width, setWithd] = useState(200)
+    const [circle, setCicle] = useState(10)
+    const changeColor = (e) => {
+        setColor(e.target.value)
+    }
+
+
+
+
+    const changeHeight = (e) => {
+        setHeight(e.target.value)
+        console.log(e.target.value)
+    }
+
+
+
+
+    const changeWidth = (e) => {
+        setWithd(e.target.value)
+        console.log(e.target.value)
+    }
+    const makeCicle = (e) => {
+        setCicle(e.target.value)
+        console.log(e.target.value)
+    }
+
+
+
+
+    return (
+        <div>
+            <input onChange={changeColor} placeholder="change color" />
+            <input onChange={changeHeight} placeholder="change color" />
+            <input onChange={changeWidth} placeholder="change color" />
+            <input onChange={makeCicle} placeholder="change color" />
+            <div style={{ height: height + 'px', width: width + 'px', backgroundColor: color, borderRadius: circle + '%' }}>
+
+
+
+
+            </div>
+        </div>
+    )
+}
+
+
+export default App;
